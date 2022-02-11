b5511fdc9f3604f02a335eda76fcba800289daff
linuxbeans <97660422+linuxbeans@users.noreply.github.com>
2022-02-11 18:05:09 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/Cluster0",{
+mongoose.connect("mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement",{
    
     useNewUrlParser:true,
     useUnifiedTopology:true
