3a294cbbbf989ee372188ff057efd99e929267e8
linuxbeans <97660422+linuxbeans@users.noreply.github.com>
2022-02-15 16:08:15 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb://new-user_31:pvI6pToIv7vLmS2Z@cluster0-shard-00-00.fnjb9.mongodb.net:27017,cluster0-shard-00-01.fnjb9.mongodb.net:27017,cluster0-shard-00-02.fnjb9.mongodb.net:27017/newmanagement?ssl=true&replicaSet=atlas-w2dcki-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const url = `mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement?retryWrites=true&w=majority`;
 
 const connectionParams={
-   useNewUrlParser:true,
-    useUnifiedTopology:true
+   useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 }
 mongoose.connect(url,connectionParams)
     .then( () => {
