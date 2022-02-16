28b46ef666997858645eacfd8b79661c2fc095e4
linuxbeans <97660422+linuxbeans@users.noreply.github.com>
2022-02-16 12:54:14 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,8 @@
-const mongoose = require('mongoose')
-
-const url = `mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement?retryWrites=true&w=majority`;
-
-const connectionParams={
-   useNewUrlParser: true,
-    useCreateIndex: false,
-    useUnifiedTopology: true
-}
-mongoose.connect(url,connectionParams)
-    .then( () => {
-        console.log('Connected to database ')
-    })
-    .catch( (err) => {
-        console.error(`Error connecting to the database. \n${err}`);
-    })
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+const collection = client.db("test").collection("devices");
+// perform actions on the collection object
+client.close();
+});
