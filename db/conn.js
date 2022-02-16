0c2a6c6ed386a8956f07875639ac35589432b997
linuxbeans <97660422+linuxbeans@users.noreply.github.com>
2022-02-16 17:15:54 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement?retryWrites=true&w=majority";
+const uri = "mongodb+srv://new-user_31:<pvI6pToIv7vLmS2Z>@cluster0.fnjb9.mongodb.net/newmanagement?retryWrites=true&w=majorityssl=true";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
 const collection = client.db("test").collection("devices");
