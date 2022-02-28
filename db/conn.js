364987418a89962af88c8fdcfeabb680c6e0cf52
linuxbeans <97660422+linuxbeans@users.noreply.github.com>
2022-02-28 17:49:36 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://192.168.55.24:27017/test",{
+mongoose.connect("mongodb://192.168.55.24:27017/newmanagement",{
    
     useNewUrlParser:true,
     useUnifiedTopology:true
